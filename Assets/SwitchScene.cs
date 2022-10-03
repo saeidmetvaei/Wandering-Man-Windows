a98552da9d315da2e61fbc60fad063419d6b3c34
Saeid Metvaei <saeidmetvaei@yahoo.com>
2022-10-04 02:29:05 +0330

    files update

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    private Dictionary<string, Action> CommandAction = new Dictionary<string, Action>();
+    private KeywordRecognizer CommandRecognizer;
+
+	// Backfround Sound sources
+    public AudioSource Audiosource;
+    public AudioClip Song;
+
+        void Start()
+    {
+
+        Audiosource.clip = Song;
+        Audiosource.Play();
+        //Audiosource.PlayOneShot(Song);
+
+
+        CommandAction.Add("change", ChangeCommand);
+
+        CommandRecognizer = new KeywordRecognizer(CommandAction.Keys.ToArray());
+        CommandRecognizer.OnPhraseRecognized += OnCommandRecognized;
+        CommandRecognizer.Start();
+
+    }
+
+    private void OnCommandRecognized(PhraseRecognizedEventArgs args)
+    {
+
+        CommandAction[args.text].Invoke();
+    }
+
+    private void ChangeCommand()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Command Recognized!");
+    }
+}
---
+++ Assets/SwitchScene2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene2 : MonoBehaviour
+{
+
+    private Dictionary<string, Action> CommandAction = new Dictionary<string, Action>();
+    private KeywordRecognizer CommandRecognizer;
+
+    public AudioSource Audiosource;
+    public AudioClip Song;
+
+    void Start()
+    {
+
+        Audiosource.clip = Song;
+        Audiosource.Play();
+        //Audiosource.PlayOneShot(Song);
+
+        CommandAction.Add("change", ChangeCommand);
+
+        CommandRecognizer = new KeywordRecognizer(CommandAction.Keys.ToArray());
+        CommandRecognizer.OnPhraseRecognized += OnCommandRecognized;
+        CommandRecognizer.Start();
+
+    }
+
+    private void OnCommandRecognized(PhraseRecognizedEventArgs args)
+    {
+
+        CommandAction[args.text].Invoke();
+    }
+
+    private void ChangeCommand()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Command Recognized!");
+    }
+}
